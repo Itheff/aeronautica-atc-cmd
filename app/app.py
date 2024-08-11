6c3766961613e106fc7552c737e6b4ba033cb4ba
Itheff <bnagyva@gmail.com>
2024-08-11 15:41:20 -0400

    Added more complex app structure

--- app/app.py
+++ app/app.py
@@ -1,15 +1,43 @@
-import tkinter as tk
+from tkinter import Tk
+from tkinter.ttk import Notebook, Frame
 
 
 def main() -> None:
-    root: tk.Tk = tk.Tk()
-    setup(root)
+    root: Tk = Tk()
+    notebook: Notebook = Notebook(root)
+    setup(root, notebook)
+    root.mainloop()
 
 
-def setup(root: tk.Tk) -> None:
+def setup(root: Tk, notebook: Notebook) -> None:
     root.geometry('512x512')
     root.resizable(False, False)
     root.title("Aeronautica ATC")
+    setup_frames()
+    notebook.pack(fill='both', expand=True)
+
+
+def setup_frames() -> dict[str, Frame]:
+    frames: dict[str, Frame] = {
+        "faa": Frame(),
+        "caa": Frame(),
+        "icao": Frame()}
+    setup_faa(frames["faa"])
+    setup_caa(frames["caa"])
+    setup_icao(frames["icao"])
+    return frames
+
+
+def setup_faa(frame: Frame):
+    pass  # TODO put main functions of app here
+
+
+def setup_caa(frame: Frame):
+    pass  # TODO put main functions of app here
+
+
+def setup_icao(frame: Frame):
+    pass  # TODO put main functions of app here
 
 
 if __name__ == '__main__':
