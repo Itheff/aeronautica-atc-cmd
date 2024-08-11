3301c9a604753220cc7139fdefd90c7f0d9be3fb
Itheff <bnagyva@gmail.com>
2024-08-11 16:19:08 -0400

    Changed the structure to add the frames to tabs

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
-from tkinter import Tk
-from tkinter.ttk import Notebook, Frame
+from tkinter import Tk, Frame
+from tkinter.ttk import Notebook
 
 
 def main() -> None:
@@ -13,30 +13,32 @@ def setup(root: Tk, notebook: Notebook) -> None:
     root.geometry('512x512')
     root.resizable(False, False)
     root.title("Aeronautica ATC")
-    setup_frames()
-    notebook.pack(fill='both', expand=True)
+    frames: dict[str, Frame] = setup_frames()
+    for key in frames.keys():
+        notebook.add(frames[key], text=key)
+    notebook.pack(fill='both')
 
 
 def setup_frames() -> dict[str, Frame]:
     frames: dict[str, Frame] = {
-        "faa": Frame(),
-        "caa": Frame(),
-        "icao": Frame()}
-    setup_faa(frames["faa"])
-    setup_caa(frames["caa"])
-    setup_icao(frames["icao"])
+        "FAA": Frame(),
+        "CAA": Frame(),
+        "ICAO": Frame()}
+    setup_faa(frames["FAA"])
+    setup_caa(frames["CAA"])
+    setup_icao(frames["ICAO"])
     return frames
 
 
-def setup_faa(frame: Frame):
+def setup_faa(frame: Frame) -> None:
     pass  # TODO put main functions of app here
 
 
-def setup_caa(frame: Frame):
+def setup_caa(frame: Frame) -> None:
     pass  # TODO put main functions of app here
 
 
-def setup_icao(frame: Frame):
+def setup_icao(frame: Frame) -> None:
     pass  # TODO put main functions of app here
 
 
