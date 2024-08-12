ab3b99a5c73a7e523d37f37ffe9e70eedf04dd10
Itheff <bnagyva@gmail.com>
2024-08-11 21:30:40 -0400

    Rearranged functional structure and added basic UI elements

--- app/app.py
+++ app/app.py
@@ -1,45 +1,51 @@
-from tkinter import Tk, Frame
+from tkinter import StringVar, Tk, Frame, Text, Button, Message
 from tkinter.ttk import Notebook
 
 
 def main() -> None:
+
+    def setup() -> None:
+        root.geometry('512x512')
+        root.resizable(False, False)
+        root.title("Aeronautica ATC")
+        frames: dict[str, Frame] = setup_frames()
+        for key in frames.keys():
+            notebook.add(frames[key], text=key)
+        notebook.pack(fill='both')
+
+    def setup_frames() -> dict[str, Frame]:
+        # Inserting the frames into a dictionary allows using a loop in the setup function above
+        frames: dict[str, Frame] = {
+            "FAA": Frame(notebook),
+            "CAA": Frame(notebook),
+            "ICAO": Frame(notebook)}
+        setup_faa(frames["FAA"])
+        setup_caa(frames["CAA"])
+        setup_icao(frames["ICAO"])
+        return frames
+
+    def setup_faa(frame: Frame) -> None:
+
+        text: Text = Text(frame, height=10, borderwidth=2)
+        submit_button = Button(frame, text="Submit")
+        message = Text(frame, height=10, borderwidth=2, wrap="word")
+        message.insert(1.0, "test")
+        message.config(state="disabled")
+        frame.pack()
+        text.pack(padx=10, pady=10)
+        submit_button.pack()
+        message.pack(padx=10, pady=10, expand=True)
+
+    def setup_caa(frame: Frame) -> None:
+        pass  # TODO put main functions of app here
+
+    def setup_icao(frame: Frame) -> None:
+        pass  # TODO put main functions of app here
+
     root: Tk = Tk()
     notebook: Notebook = Notebook(root)
-    setup(root, notebook)
+    setup()
     root.mainloop()
-
-
-def setup(root: Tk, notebook: Notebook) -> None:
-    root.geometry('512x512')
-    root.resizable(False, False)
-    root.title("Aeronautica ATC")
-    frames: dict[str, Frame] = setup_frames()
-    for key in frames.keys():
-        notebook.add(frames[key], text=key)
-    notebook.pack(fill='both')
-
-
-def setup_frames() -> dict[str, Frame]:
-    frames: dict[str, Frame] = {
-        "FAA": Frame(),
-        "CAA": Frame(),
-        "ICAO": Frame()}
-    setup_faa(frames["FAA"])
-    setup_caa(frames["CAA"])
-    setup_icao(frames["ICAO"])
-    return frames
-
-
-def setup_faa(frame: Frame) -> None:
-    pass  # TODO put main functions of app here
-
-
-def setup_caa(frame: Frame) -> None:
-    pass  # TODO put main functions of app here
-
-
-def setup_icao(frame: Frame) -> None:
-    pass  # TODO put main functions of app here
 
 
 if __name__ == '__main__':
