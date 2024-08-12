9011436bca5e269a46982b4f0b82f9175a874a2e
Itheff <bnagyva@gmail.com>
2024-08-12 00:01:18 -0400

    FAA tab is fully functional but still useless for now, needs the flightplan to be recompiled and then it's done.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,11 @@
-from tkinter import END, Tk, Frame, Text, Button, Widget, StringVar
+from tkinter import END, Tk, Frame, Text, Button, Widget
 from tkinter.ttk import Notebook
-from typing import Callable
+from typing import Callable, List
+
+
+# Total stack overflow tabs open concurrently: 7
+# % of code ripped from ChatGPT: 0
+# Don't do AI, kids.
 
 
 def main() -> None:
@@ -26,20 +31,57 @@ def main() -> None:
             "CAA": Frame(notebook),
             "ICAO": Frame(notebook)}
 
+        def split_flight_plan(flight_plan: str) -> List[str]:
+            # This function is quite verbose and could likely be done better... but I'm not going to.
+            # All you need to know is that it removes unnecessary characters and splits the string into a list.
+            flight_plan = flight_plan.replace("\"", "\t")
+            flight_plan = flight_plan.replace("/", "\t")
+            flight_plan = flight_plan.replace(" ", "")
+            flight_plan = flight_plan.replace("\n", "\t")
+            local_split_flight_plan = flight_plan.split("\t")
+            # This mess is to remove empty list items without crashing. I don't know of a better way to do this.
+            to_remove: List[int] = []
+            for i in range(len(local_split_flight_plan)):
+                if local_split_flight_plan[i] == "\t" or local_split_flight_plan[i] == "":
+                    to_remove.append(i)
+            to_remove.reverse()
+            for i in to_remove:
+                local_split_flight_plan.pop(i)
+            return local_split_flight_plan
+
         def submit_faa() -> None:
+
+            def generate_faa_clearance(flight_plan: str) -> str:
+                local_split_flight_plan: List[str] = split_flight_plan(flight_plan)
+                match local_split_flight_plan[0]:
+                    case "DEPARTURE":
+                        pass
+                    case "ARRIVAL":
+                        pass
+                    case "ENROUTE":
+                        pass
+                    case "EMERGENCY":
+                        pass
+                    case _:
+                        local_split_flight_plan.insert(0, "")
+                print(local_split_flight_plan)
+                return "Flight plan received"  # TODO Bring it all together here, its late and I'm going to bed
+
             children: list[Widget] = frames["FAA"].winfo_children()
             try:
                 # This is some silly JavaScript type stuff but I know the rules therefor I'm allowed to break them.
                 # Seriously though this is really awful and unsafe and you want type consistency don't do this.
                 # But hey it's in a try except block and you can do anything you want in those... right?
                 # I just found out I can do this too... I forsee many crashed programs in my future
-                text: Text = children[2]
+                text: Text = children[2]  # Pyright really hates this and I kinda do too, oh well.
                 text.config(state="normal")
                 text.delete(1.0, END)
-                text.insert(1.0, faa_clearance_var.get())
+                input_field: Text = children[0]  # This too.
+                text.insert(1.0, generate_faa_clearance(input_field.get("1.0", END)))  # I love one-liners.
                 text.config(state="disabled")
-            except:
+            except Exception as e:
                 print("Error: Problem generating clearance")
+                print(e)
 
         def submit_caa() -> None:
             pass
@@ -68,9 +110,6 @@ def main() -> None:
         message.pack(padx=10, pady=10, expand=True)
 
     root: Tk = Tk()
-    faa_clearance_var: StringVar = StringVar(root, "test2")
-    caa_clearance_var: StringVar = StringVar(root, "")
-    icao_clearance_var: StringVar = StringVar(root, "")
     notebook: Notebook = Notebook(root)
     setup()
     root.mainloop()
