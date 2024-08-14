649edbf5fa4d6436f9fa33cab24c5e6c852e3e00
Itheff <bnagyva@gmail.com>
2024-08-13 21:10:44 -0400

    Added frequencies and squawk codes

--- app/app.py
+++ app/app.py
@@ -1,12 +1,50 @@
+import random
 import traceback
 from tkinter import END, Tk, Frame, Text, Button, Widget
 from tkinter.ttk import Notebook
-from typing import Callable, List, Literal
+from typing import Callable, List, Literal, TextIO
 
 
 # Total stack overflow tabs open concurrently: 7
 # % of code ripped from ChatGPT: 0
 # Don't do AI, kids.
+
+
+class CSVFile:
+    """Important note, this class assumes the CSV file has a header"""
+
+    raw_csv_file: TextIO
+    content: List[dict[str, str]] = []
+
+    def __init__(self, csv_file: TextIO):
+        #  I'm really proud of this code, its perfectly type safe yet wonderfully pythony
+        #  (Yes I know the more pythony thing would have been to import the csv library but let me have this)
+        self.raw_csv_file = csv_file
+        lines: List[str] = csv_file.read().split("\n")
+        headers = lines[0].split(",")
+        for line in lines[1:]:
+            split_line = line.split(",")
+            temp_dict: dict[str, str] = {}
+            for i in range(len(split_line)):
+                temp_dict.update({headers[i]: split_line[i]})
+            self.content.append(temp_dict)
+
+    def find_airport(self, icao: str) -> dict[str, str]:
+        try:
+            for row in self.content:
+                if row["ICAO"] == icao:
+                    return row
+            raise Exception("ICAO code does not exist or frequencies database is corrupted")
+        except Exception as e:
+            print(e)
+            return {"ICAO": "ERROR",
+                    "Name": "ERROR",
+                    "DEL": "ERROR",
+                    "GND": "ERROR",
+                    "TWR": "ERROR",
+                    "DEP": "ERROR",
+                    "APP": "ERROR",
+                    "CTR": "ERROR"}
 
 
 class FlightPlan:
@@ -22,15 +60,19 @@ class FlightPlan:
     arrival: str
     route: str
     remarks: str
+    frequencies: CSVFile
+    #  Can you tell I'm used to Java?
 
     def __init__(self, raw_flight_plan: str):
 
+        #  This removes a bunch of unnecessary characters from the flight plan
         raw_flight_plan = raw_flight_plan
         raw_flight_plan = raw_flight_plan.replace("\"", "\t")
         raw_flight_plan = raw_flight_plan.replace("/", "\t")
         raw_flight_plan = raw_flight_plan.replace(" ", "")
         raw_flight_plan = raw_flight_plan.replace("\n", "\t")
         split_flight_plan = raw_flight_plan.split("\t")
+
         # This mess is to remove empty list items without crashing. I don't know of a better way to do this.
         to_remove: List[int] = []
         for i in range(len(split_flight_plan)):
@@ -39,6 +81,8 @@ class FlightPlan:
         to_remove.reverse()
         for i in to_remove:
             split_flight_plan.pop(i)
+
+        #  This makes sure that the potentially empty first element is always present for consistent list ordering
         match split_flight_plan[0]:
             case "DEPARTURE":
                 pass
@@ -62,20 +106,25 @@ class FlightPlan:
         self.arrival = split_flight_plan[9].upper()
         self.route = split_flight_plan[10].upper()
         self.remarks = split_flight_plan[11].upper()
+        self.frequencies = CSVFile(open("resources/frequencies_database.csv", "r"))
 
     def generate_squawk(self) -> None:
-        self.squawk_code = "1200"  # TODO IMPLEMENT THIS
+        temp = ""
+        for i in range(0, 4):
+            temp += str(random.randint(0, 7))
+        self.squawk_code = temp
 
     def find_frequency(self, airport: str, frequency_type: Literal["del", "gnd", "twr", "app", "dep", "ctr"]) -> str:
-        return "122.800"
+        airport_dict: dict[str, str] = self.frequencies.find_airport(airport)
+        return airport_dict[frequency_type.upper()]
 
     def to_string(self, phraseology: Literal["faa", "caa", "icao"]) -> str:
         match phraseology:
             case "faa":
                 return_string: str = (f"CLR {self.radio_call_sign} TO ARR {self.arrival} VIA {self.route}. RMK INITIAL "
                                       f"ALT 4000 EXPECT {self.requested_flight_level} 10 MINS AFT DEP. DEP FREQ ON "
-                                      f"{self.find_frequency(self.departure, 'dep')} CTC GND ON "
-                                      f"{self.find_frequency(self.departure, 'gnd')} FOR PUSH AND TAXI.")
+                                      f"{self.find_frequency(self.departure, 'dep')} SQUAWK {self.squawk_code}. "
+                                      f"CTC GND ON {self.find_frequency(self.departure, 'gnd')} FOR PUSH AND TAXI.")
                 return return_string
             case "caa":
                 return "NOT IMPLEMENTED"  # TODO IMPLEMENT THIS
@@ -84,10 +133,6 @@ class FlightPlan:
 
 
 def main() -> None:
-
-    # Only but a humble debug function, pay him no mind
-    def do_nothing() -> None:
-        print(":D")
 
     def setup() -> None:
         # This function is pretty self-explanatory... it sets up the ui
@@ -116,7 +161,7 @@ def main() -> None:
 
         def submit(phraseology: Literal["faa", "caa", "icao"]) -> None:
             # This is not type safe at all but I know the rules therefore I'm allowed to break them
-            children: List[Widget] = frames["FAA"].winfo_children()
+            children: List[Widget] = frames[phraseology.upper()].winfo_children()
             try:
                 text: Text = children[2]  # Pyright really hates this and I kinda do too, oh well.
                 clear_text_box(text)
@@ -130,10 +175,9 @@ def main() -> None:
 
         # These function calls create the tabs. The only thing different between them is the output when the button
         # is pressed.
-        # TODO TOP PRIORITY, FIX THIS FIRST
-        setup_mode(frames["FAA"], lambda: submit("faa"))  # TODO THIS 100% NOT WORKING AS INTENDED
-        setup_mode(frames["CAA"], lambda: submit("caa"))  # TODO ITS A VERY EASY (haha) FIX MOVE THE SUBMIT FUNCTION
-        setup_mode(frames["ICAO"], lambda: submit("icao"))  # TODO TO AN OUTER SCOPE BUT IM GOING TO BED NOW
+        setup_mode(frames["FAA"], lambda: submit("faa"))
+        setup_mode(frames["CAA"], lambda: submit("caa"))
+        setup_mode(frames["ICAO"], lambda: submit("icao"))
 
         return frames
 
